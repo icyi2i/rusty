b8844d855b71ea1fc9b7dc268fe70a4bf40ff606
Sarthak Gambhir <sarthak3gambhir@gmail.com>
2023-01-03 19:43:12 +0000

    Add initial hello world app to project learning library.

---
+++ learn/hello_rusty/src/main.rs
@@ -0,0 +1,6 @@
+// Definition of main function
+// Main function of main.rs is called when a project is ran
+fn main() {
+    // println prints the content to the console.
+    println!("Hello, Rusty!");
+}
