e81e252fe8a20356356fdae70014eea539b55750
Sarthak Gambhir <sarthak3gambhir@gmail.com>
2023-01-05 17:50:49 +0000

    Extend tuple example. Add example for arrays.

--- learn/datatypes_compound/src/main.rs
+++ learn/datatypes_compound/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    // Tuples are groups of data which is immutable.
+    // Tuples are immutable collections of data.
+    // Tuples can be heterogenous.
     let shape_data = ("Square", 4, true);
     // Data is accessed from a tuple using <tuple>.<index> notation
     println!(
@@ -12,4 +13,19 @@ fn main() {
         "{} has {} sides. Is it a quadrilateral? : {}",
         shape, sides_count, is_quadrilateral
     );
+    // Tuples structure itself is immutable.
+    // But the inner values can be made mutable
+    let mut shape_data = ("Square", 4, true);
+    println!("Old value : {}", shape_data.0);
+    // Here element at index 0 will remain a string. Datatype can't be changed.
+    shape_data.0 = "Rectangle";
+    println!("New value : {}", shape_data.0);
+
+    // Arrays are collection of homogenous data elements
+    // All elements are supposed to be of same datatype
+    let data_arr = [1, 2, 3, 4];
+    println!(
+        "Array contains : {} {} {} {}",
+        data_arr[0], data_arr[1], data_arr[2], data_arr[3],
+    )
 }
